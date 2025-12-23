d7c526f3b25079ad052d4dd4da2ac14677b6d409
yanarththani <kyanarththanik@gmail.com>
2025-12-23 10:53:20 +0530

    Fix: Track fincore-app as regular directory

---
+++ fincore-app/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import BranchManagementPage from "./branches/page";
+
+export default function Home() {
+  return (
+   <div>
+    <BranchManagementPage />
+   </div>
+ 
+  );
+}
